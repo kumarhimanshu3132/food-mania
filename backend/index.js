fd02c15bf71b24aa8bbd497b473cf4fe79496dd2
Himanshu Kumar <kumarhimanshu3132@gmail.com>
2026-04-05 15:09:23 +0530

    Final fix: Open CORS for Vercel deployment

--- backend/index.js
+++ backend/index.js
@@ -6,13 +6,15 @@ const mongoDB = require("./db");
 mongoDB();
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept",
   );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   next();
 });
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
